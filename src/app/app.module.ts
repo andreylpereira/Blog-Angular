fd8648f57cc1e91fbabefa9b3af8e2a38b28074f
Andrey Luiz Pereira <andreylpereira@gmail.com>
2022-04-01 19:00:05 -0300

    Interceptor de token para utlização no cabeçalho dos formulários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
-import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import {
+  JwtHelperService,
+  JWT_OPTIONS,
+} from '@auth0/angular-jwt';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagesModule } from 'src/app/pages/pages.module';
 import { ControlPanelRoutingModule } from './pages/control-panel/control-panel-routing';
 import { AppComponent } from './app.component';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from 'src/app/_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,11 +21,12 @@ import { AppComponent } from './app.component';
     ControlPanelRoutingModule,
     PagesModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
   ],
   providers: [
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,17 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ControlPanelModule } from 'src/app/pages/control-panel/control-panel.module';
+import { NgxPaginationModule } from 'ngx-pagination';
 /* Components */
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { CategoriesComponent } from './home/categories/categories.component';
 import { ArticlesComponent } from './home/articles/articles.component';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { CategoryArticlesComponent } from './category-articles/category-articles.component';
+import { DetailArticleComponent } from './home/detail-article/detail-article.component'
 /* Pipe */
 import { SafeHtmlPipe } from '../pipes/safe-html.pipe';
-import { DetailArticleComponent } from './home/detail-article/detail-article.component'
 
 @NgModule({
   declarations: [LoginComponent, HomeComponent, AboutComponent, CategoriesComponent, ArticlesComponent, CategoryArticlesComponent, SafeHtmlPipe, DetailArticleComponent],
